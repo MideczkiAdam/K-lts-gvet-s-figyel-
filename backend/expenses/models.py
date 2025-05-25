f5a2eaed94ff6cc48fc6505ac6719b8df00667c3
Zoli0217 <zoltanbodi61@gmail.com>
2025-05-25 20:20:57 +0200

    add income backend

--- backend/expenses/models.py
+++ backend/expenses/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Transaction(models.Model):
+    TRANSACTION_TYPES = [
+        ('income', 'Bevétel'),
+        ('expense', 'Kiadás'),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    type = models.CharField(choices=TRANSACTION_TYPES, max_length=10)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.type} - {self.amount} Ft"
